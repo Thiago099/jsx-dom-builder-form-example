8e9f736ac8ad4d5634473e9c9d6c675bf24a097b
Thiago <thiago.kaique@stayfilm.com.br>
2023-01-26 19:54:10 -0300

    feature

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,8 @@ var data = effect(
     age:20
 });
 
+console.log(JSON.stringify(data))
+
 function submit()
 {
     alert("Submit logic here")
@@ -24,13 +26,13 @@ const app =
                 <input type="text" model={data.age}></input>
             </div>
         </div>
-        <div class="tooltip">The model parameter makes the input in sync with any variable</div>
+        <div class="tooltip">The model parameter makes the input in sync with any variable.</div>
     </div>
     <div class="card">
         <div> Name: {data.name} </div>
         <div> Age: {data.age} </div>
     </div>
-    <div class="tooltip">You can also add them directaly in the element that they will update either using effect or manually using the "element.$update()"</div>
+    <div class="tooltip">You can also add them directly in the element that they will update either using effect or manually using the "element.$update()".</div>
     <div class="footer-button-container">
         <button on:click={submit}> Submit </button>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     plugins:[
         jsxDomBuilderVitePlugin(),
     ],
+    //base: '/jsx-dom-builder-form-example/', 
     // make the @ as a alias to the src folder (opitional but recomended)
     resolve: {
         alias: {
