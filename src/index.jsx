ddc901fb62faa26fee79e27d7a9abdbf90960846
Thiago <thiago.kaique@stayfilm.com.br>
2023-01-26 19:42:46 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,39 @@
+import './style.css'
+
+var data = effect(
+{
+    name:"pedro",
+    age:20
+});
+
+function submit()
+{
+    alert("Submit logic here")
+}
+
+const app = 
+<div effect={data} class="main-container">
+    <div>
+        <div class="input-group">
+            <div class="input-container half">
+                <label>Name:</label>
+                <input type="text" model={data.name}></input>
+            </div>
+            <div class="input-container half">
+                <label>Age:</label>
+                <input type="text" model={data.age}></input>
+            </div>
+        </div>
+        <div class="tooltip">The model parameter makes the input in sync with any variable</div>
+    </div>
+    <div class="card">
+        <div> Name: {data.name} </div>
+        <div> Age: {data.age} </div>
+    </div>
+    <div class="tooltip">You can also add them directaly in the element that they will update either using effect or manually using the "element.$update()"</div>
+    <div class="footer-button-container">
+        <button on:click={submit}> Submit </button>
+    </div>
+</div>
+
+app.$parent(document.body)
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite"
+import  jsxDomBuilderVitePlugin  from "jsx-dom-builder/vite-plugin"
+
+
+
+
+export default defineConfig({
+    plugins:[
+        jsxDomBuilderVitePlugin(),
+    ],
+    // make the @ as a alias to the src folder (opitional but recomended)
+    resolve: {
+        alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        }
+    }
+})
